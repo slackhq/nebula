8c65d969189c6f24cfe24590e7324946db71855f
alanhlam <alanhoanglam@gmail.com>
2019-11-21 22:20:08 -0800

    make this py3 compatible (#22)
    * make this py3 compatible

--- examples/quickstart-vagrant/ansible/filter_plugins/to_nebula_ip.py
+++ examples/quickstart-vagrant/ansible/filter_plugins/to_nebula_ip.py
@@ -9,13 +9,13 @@ class FilterModule(object):
         }
 
     def to_nebula_ip(self, ip_str):
-        ip_list = map(int, ip_str.split("."))
+        ip_list = list(map(int, ip_str.split(".")))
         ip_list[0] = 10
         ip_list[1] = 168
         ip = '.'.join(map(str, ip_list))
         return ip
 
     def map_to_nebula_ips(self, ip_strs):
-	ip_list = [ self.to_nebula_ip(ip_str) for ip_str in ip_strs ]
-	ips = ', '.join(ip_list)
+        ip_list = [ self.to_nebula_ip(ip_str) for ip_str in ip_strs ]
+        ips = ', '.join(ip_list)
         return ips
