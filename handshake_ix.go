baa45d7ecdeaee5f98ae6fdbb9fff41948ea4c54
Ryan Huber <rhuber@gmail.com>
2019-11-24 03:42:34 +0000

    clean up

--- handshake_ix.go
+++ handshake_ix.go
@@ -74,13 +74,10 @@ func ixHandshakeStage1(f *Interface, addr *udpAddr, hostinfo *HostInfo, packet [
 	var ip uint32
 	if h.RemoteIndex == 0 {
 		ci := f.newConnectionState(false, noise.HandshakeIX, f.PSK, 0)
-		// Mark packet 1 as seen so it doesn't show up as missed
-
 		msg, _, _, err := ci.H.ReadMessage(nil, packet[HeaderLen:])
 		if err != nil && len(f.AltPSKs) > 0 {
 			l.WithError(err).WithField("udpAddr", addr).
 				WithField("handshake", m{"stage": 1, "style": "ix_psk0"}).Error("Failed to call noise.ReadMessage")
-			//return true
 			for _, key := range f.AltPSKs {
 				ci = f.newConnectionState(false, noise.HandshakeIX, key, 0)
 
@@ -92,7 +89,7 @@ func ixHandshakeStage1(f *Interface, addr *udpAddr, hostinfo *HostInfo, packet [
 					break
 				}
 			}
-			//return true
+			return true
 		}
 		// Mark packet 1 as seen so it doesn't show up as missed
 		ci.window.Update(1)
