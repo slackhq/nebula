c321e40d24b7e66f451ad48ebc9cc834b11eeb45
Wade Simmons <wade@wades.im>
2019-12-17 08:38:59 -0500

    generate the smoke config files

--- .github/workflows/smoke/build.sh
+++ .github/workflows/smoke/build.sh
@@ -10,7 +10,10 @@ mkdir ./build
 
     cp ../../../../nebula .
     cp ../../../../nebula-cert .
-    cp ../*.yml .
+
+    HOST="lighthouse1" AM_LIGHTHOUSE=true ../genconfig.sh >lighthouse1.yml
+    HOST="host2" LIGHTHOUSES="192.168.100.1 172.17.0.2:4242" ../genconfig.sh >host2.yml
+    HOST="host3" LIGHTHOUSES="192.168.100.1 172.17.0.2:4242" ../genconfig.sh >host3.yml
 
     ./nebula-cert ca -name "Smoke Test"
     ./nebula-cert sign -name "lighthouse1" -ip "192.168.100.1/24"
