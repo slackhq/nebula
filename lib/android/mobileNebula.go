a2b28196ce2d47c3d0ed645ecd511a25feabcdc6
Ryan Huber <rhuber@gmail.com>
2019-12-25 12:58:00 +0000

    bubble error up

--- lib/android/mobileNebula.go
+++ lib/android/mobileNebula.go
@@ -1,9 +1,11 @@
 package mobileNebula
 
 import (
+	"fmt"
 	"github.com/slackhq/nebula"
 )
 
-func Main(configData string, tunFd int) {
-	nebula.Main(configData, false, "", &tunFd)
+func Main(configData string, tunFd int) string {
+	err := nebula.Main(configData, false, "", &tunFd)
+  return fmt.Sprintf("%s", err)
 }
