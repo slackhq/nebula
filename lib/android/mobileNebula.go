7cb213cc18f00be7124e2899954ea8f405ab187d
Ryan Huber <rhuber@gmail.com>
2019-12-27 03:44:40 +0000

    lame way to get ip/mask

--- lib/android/mobileNebula.go
+++ lib/android/mobileNebula.go
@@ -3,9 +3,44 @@ package mobileNebula
 import (
 	"fmt"
 	"github.com/slackhq/nebula"
+	"github.com/slackhq/nebula/cert"
 )
 
 func Main(configData string, tunFd int) string {
 	err := nebula.Main(configData, false, "", &tunFd)
-  return fmt.Sprintf("%s", err)
+	return fmt.Sprintf("%s", err)
+}
+
+func GetConfigSetting(configData string, setting string) string {
+	config := nebula.NewConfig()
+	config.LoadString(configData)
+	return config.GetString(setting, "")
+}
+
+func GetHostCertIP(configData string) (string) {
+	c := GetConfigSetting(configData, "pki.cert")
+	rawCert := []byte(c)
+	crt, _, err := cert.UnmarshalNebulaCertificateFromPEM(rawCert)
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+
+	return crt.Details.Ips[0].IP.String()
+	//return "HI"
+}
+
+func GetHostCertMask(configData string) (int) {
+	c := GetConfigSetting(configData, "pki.cert")
+	rawCert := []byte(c)
+	crt, _, err := cert.UnmarshalNebulaCertificateFromPEM(rawCert)
+	if err != nil {
+		fmt.Println(err)
+		return 0 
+	}
+
+  _, pre := crt.Details.Ips[0].Mask.Size()
+
+	return pre
+	//return "HI"
 }
