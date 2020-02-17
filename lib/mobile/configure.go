5503b7b0c3787004e03dbec7df659e9ebca9727f
Ryan Huber <rhuber@gmail.com>
2020-02-17 14:38:14 -0600

    gofmt fix

--- lib/mobile/configure.go
+++ lib/mobile/configure.go
@@ -1,20 +1,20 @@
 package mobileNebula
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
 var msg = "empty"
 
 func ConfigHTTPServer() {
-    fmt.Println("webserver start")
-    http.HandleFunc("/", HelloServer)
-    http.ListenAndServe(":8080", nil)
+	fmt.Println("webserver start")
+	http.HandleFunc("/", HelloServer)
+	http.ListenAndServe(":8080", nil)
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Howdy, %s\n%s!", r.RemoteAddr, msg)
+	fmt.Fprintf(w, "Howdy, %s\n%s!", r.RemoteAddr, msg)
 }
 
 func SetMessage(m string) {
