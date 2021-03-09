f57499efd8ccda8d006d27dc6474914bae7a5dbf
Wade Simmons <wade@wades.im>
2021-03-09 14:09:16 -0500

    more allow_list tests

--- allow_list_test.go
+++ allow_list_test.go
@@ -14,7 +14,10 @@ func TestAllowList_Allow(t *testing.T) {
 	tree := NewCIDR6Tree()
 	tree.AddCIDR(getCIDR("0.0.0.0/0"), true)
 	tree.AddCIDR(getCIDR("10.0.0.0/8"), false)
+	tree.AddCIDR(getCIDR("10.42.42.42/32"), true)
+	tree.AddCIDR(getCIDR("10.42.0.0/16"), true)
 	tree.AddCIDR(getCIDR("10.42.42.0/24"), true)
+	tree.AddCIDR(getCIDR("10.42.42.0/24"), false)
 	tree.AddCIDR(getCIDR("::1/128"), true)
 	tree.AddCIDR(getCIDR("::2/128"), false)
 	al := &AllowList{cidrTree: tree}
@@ -22,6 +25,8 @@ func TestAllowList_Allow(t *testing.T) {
 	assert.Equal(t, true, al.Allow(net.ParseIP("1.1.1.1")))
 	assert.Equal(t, false, al.Allow(net.ParseIP("10.0.0.4")))
 	assert.Equal(t, true, al.Allow(net.ParseIP("10.42.42.42")))
+	assert.Equal(t, false, al.Allow(net.ParseIP("10.42.42.41")))
+	assert.Equal(t, true, al.Allow(net.ParseIP("10.42.0.1")))
 	assert.Equal(t, true, al.Allow(net.ParseIP("::1")))
 	assert.Equal(t, false, al.Allow(net.ParseIP("::2")))
 }
