3eeccef50a9bd780be65e0b450bf14f12579fcfb
Wade Simmons <wade@wades.im>
2021-11-04 22:21:09 -0400

    test that initFunc is called if hostinfo created
    Also assert that the second time we call, initFunc is not called and we
    get the same object back.

--- handshake_manager_test.go
+++ handshake_manager_test.go
@@ -27,7 +27,19 @@ func Test_NewHandshakeManagerVpnIp(t *testing.T) {
 	now := time.Now()
 	blah.NextOutboundHandshakeTimerTick(now, mw)
 
-	i := blah.AddVpnIp(ip, nil)
+	var initCalled bool
+	initFunc := func(*HostInfo) {
+		initCalled = true
+	}
+
+	i := blah.AddVpnIp(ip, initFunc)
+	assert.True(t, initCalled)
+
+	initCalled = false
+	i2 := blah.AddVpnIp(ip, initFunc)
+	assert.False(t, initCalled)
+	assert.Same(t, i, i2)
+
 	i.remotes = NewRemoteList()
 	i.HandshakeReady = true
 
