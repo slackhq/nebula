300d1bed98c20d1b446d30781a0e2d937e8475c6
Brad Higgins <brad@defined.net>
2022-06-13 11:13:56 -0400

    Configure handshake_manager to only use relays if the host is not configured as a relay

--- main.go
+++ main.go
@@ -230,11 +230,13 @@ func Main(c *config.C, configTest bool, buildVersion string, logger *logrus.Logg
 		messageMetrics = newMessageMetricsOnlyRecvError()
 	}
 
+	useRelays := c.GetBool("relay.use_relays", DefaultUseRelays) && !c.GetBool("relay.am_relay", false)
+
 	handshakeConfig := HandshakeConfig{
 		tryInterval:   c.GetDuration("handshakes.try_interval", DefaultHandshakeTryInterval),
 		retries:       c.GetInt("handshakes.retries", DefaultHandshakeRetries),
 		triggerBuffer: c.GetInt("handshakes.trigger_buffer", DefaultHandshakeTriggerBuffer),
-		useRelays:     c.GetBool("relay.use_relays", DefaultUseRelays),
+		useRelays:     useRelays,
 
 		messageMetrics: messageMetrics,
 	}
