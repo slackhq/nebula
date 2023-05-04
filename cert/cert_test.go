702e1c59bdb389ec34af76c182124281cf0b9184
Nate Brown <nbrown.us@gmail.com>
2023-05-04 16:09:42 -0500

    Always disconnect block listed hosts (#858)

--- cert/cert_test.go
+++ cert/cert_test.go
@@ -177,7 +177,7 @@ func TestNebulaCertificate_Verify(t *testing.T) {
 
 	v, err := c.Verify(time.Now(), caPool)
 	assert.False(t, v)
-	assert.EqualError(t, err, "certificate has been blocked")
+	assert.EqualError(t, err, "certificate is in the block list")
 
 	caPool.ResetCertBlocklist()
 	v, err = c.Verify(time.Now(), caPool)
