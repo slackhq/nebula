2413e2d009fd678dff6ef389d208cd1b286bda0b
Wade Simmons <wsimmons@slack-corp.com>
2023-05-04 10:32:53 -0400

    use crypto/ecdh
    This is the preferred method for doing ECDH functions now, and also has
    a boringcrypto specific codepath.

--- noiseutil/nist.go
+++ noiseutil/nist.go
@@ -1,26 +1,25 @@
 package noiseutil
 
 import (
-	"crypto/elliptic"
+	"crypto/ecdh"
 	"crypto/rand"
-	"errors"
+	"fmt"
 	"io"
 
 	"github.com/flynn/noise"
 )
 
 // DHP256 is the NIST P-256 ECDH function
-var DHP256 noise.DHFunc = newNISTCurve("P256", elliptic.P256())
+var DHP256 noise.DHFunc = newNISTCurve("P256", ecdh.P256(), 32)
 
 type nistCurve struct {
 	name   string
-	curve  elliptic.Curve
+	curve  ecdh.Curve
 	dhLen  int
 	pubLen int
 }
 
-func newNISTCurve(name string, curve elliptic.Curve) nistCurve {
-	byteLen := (curve.Params().BitSize + 7) / 8
+func newNISTCurve(name string, curve ecdh.Curve, byteLen int) nistCurve {
 	return nistCurve{
 		name:  name,
 		curve: curve,
@@ -34,26 +33,25 @@ func (c nistCurve) GenerateKeypair(rng io.Reader) (noise.DHKey, error) {
 	if rng == nil {
 		rng = rand.Reader
 	}
-	privkey, x, y, err := elliptic.GenerateKey(c.curve, rng)
+	privkey, err := c.curve.GenerateKey(rng)
 	if err != nil {
 		return noise.DHKey{}, err
 	}
-	pubkey := elliptic.Marshal(c.curve, x, y)
-	return noise.DHKey{Private: privkey, Public: pubkey}, nil
+	pubkey := privkey.PublicKey()
+	return noise.DHKey{Private: privkey.Bytes(), Public: pubkey.Bytes()}, nil
 }
 
 func (c nistCurve) DH(privkey, pubkey []byte) ([]byte, error) {
-	// based on stdlib crypto/tls/key_schedule.go
-	// - https://github.com/golang/go/blob/go1.19/src/crypto/tls/key_schedule.go#L167-L178
-	// Unmarshal also checks whether the given point is on the curve.
-	x, y := elliptic.Unmarshal(c.curve, pubkey)
-	if x == nil {
-		return nil, errors.New("unable to unmarshal pubkey")
+	ecdhPubKey, err := c.curve.NewPublicKey(pubkey)
+	if err != nil {
+		return nil, fmt.Errorf("unable to unmarshal pubkey: %w", err)
+	}
+	ecdhPrivKey, err := c.curve.NewPrivateKey(privkey)
+	if err != nil {
+		return nil, fmt.Errorf("unable to unmarshal pubkey: %w", err)
 	}
 
-	xShared, _ := c.curve.ScalarMult(x, y, privkey)
-	sharedKey := make([]byte, c.dhLen)
-	return xShared.FillBytes(sharedKey), nil
+	return ecdhPrivKey.ECDH(ecdhPubKey)
 }
 
 func (c nistCurve) DHLen() int {
