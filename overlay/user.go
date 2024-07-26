17971bcb75caef6930df5ea382bf918fab30e3ee
Ulrich Hornung <hornunguli@gmx.de>
2024-07-26 09:43:44 +0200

    Revert "Add buffering to UserDevice"
    This reverts commit 37ff25a77e28862cf9430716e78b56c2481b8f89.

--- overlay/user.go
+++ overlay/user.go
@@ -1,8 +1,6 @@
 package overlay
 
 import (
-	"bytes"
-	"fmt"
 	"io"
 	"net/netip"
 
@@ -10,68 +8,14 @@ import (
 	"github.com/slackhq/nebula/config"
 )
 
-type BufferedPipe struct {
-	close chan struct{}
-	buf   chan []byte
-}
-
-type BufferedPipeReader struct{ BufferedPipe }
-type BufferedPipeWriter struct{ BufferedPipe }
-
-func (p BufferedPipeReader) Read(data []byte) (int, error) {
-	select {
-	case d := <-p.buf:
-		{
-			copy(data, d)
-			return len(d), nil
-		}
-	case <-p.close:
-		{
-			return 0, fmt.Errorf("pipe closed")
-		}
-	}
-}
-
-func (p BufferedPipeReader) Close() error {
-	p.close <- struct{}{}
-	return nil
-}
-
-func (p BufferedPipeWriter) Write(data []byte) (int, error) {
-	c := bytes.Clone(data)
-	select {
-	case <-p.close:
-		{
-			return 0, fmt.Errorf("pipe closed")
-		}
-	case p.buf <- c:
-		{
-		}
-	}
-	return len(data), nil
-}
-
-func (p BufferedPipeWriter) Close() error {
-	p.close <- struct{}{}
-	return nil
-}
-
-func NewBufferedPipe(depth int) (BufferedPipeReader, BufferedPipeWriter) {
-	pipe := BufferedPipe{
-		make(chan struct{}),
-		make(chan []byte, depth),
-	}
-	return BufferedPipeReader{pipe}, BufferedPipeWriter{pipe}
-}
-
 func NewUserDeviceFromConfig(c *config.C, l *logrus.Logger, tunCidr netip.Prefix, routines int) (Device, error) {
 	return NewUserDevice(tunCidr)
 }
 
 func NewUserDevice(tunCidr netip.Prefix) (Device, error) {
 	// these pipes guarantee each write/read will match 1:1
-	or, ow := NewBufferedPipe(40)
-	ir, iw := NewBufferedPipe(40)
+	or, ow := io.Pipe()
+	ir, iw := io.Pipe()
 	return &UserDevice{
 		tunCidr:        tunCidr,
 		outboundReader: or,
@@ -84,11 +28,11 @@ func NewUserDevice(tunCidr netip.Prefix) (Device, error) {
 type UserDevice struct {
 	tunCidr netip.Prefix
 
-	outboundReader BufferedPipeReader
-	outboundWriter BufferedPipeWriter
+	outboundReader *io.PipeReader
+	outboundWriter *io.PipeWriter
 
-	inboundReader BufferedPipeReader
-	inboundWriter BufferedPipeWriter
+	inboundReader *io.PipeReader
+	inboundWriter *io.PipeWriter
 }
 
 func (d *UserDevice) Activate() error {
@@ -101,7 +45,7 @@ func (d *UserDevice) NewMultiQueueReader() (io.ReadWriteCloser, error) {
 	return d, nil
 }
 
-func (d *UserDevice) Pipe() (BufferedPipeReader, BufferedPipeWriter) {
+func (d *UserDevice) Pipe() (*io.PipeReader, *io.PipeWriter) {
 	return d.inboundReader, d.outboundWriter
 }
 
