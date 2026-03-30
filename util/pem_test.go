fb513ef71e45caef1801c7461116a4aa041d6819
John Maguire <contact@johnmaguire.me>
2026-03-30 15:58:35 -0400

    Fix testifylint

--- util/pem_test.go
+++ util/pem_test.go
@@ -5,7 +5,6 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -24,8 +23,8 @@ func TestSplitPEM_Single(t *testing.T) {
 	input := "-----BEGIN TEST-----\ndata\n-----END TEST-----\n"
 	blocks, err := scanAll(t, input)
 	require.NoError(t, err)
-	assert.Len(t, blocks, 1)
-	assert.Equal(t, input, blocks[0])
+	require.Len(t, blocks, 1)
+	require.Equal(t, input, blocks[0])
 }
 
 func TestSplitPEM_Multiple(t *testing.T) {
@@ -33,52 +32,52 @@ func TestSplitPEM_Multiple(t *testing.T) {
 	block2 := "-----BEGIN TEST-----\nbbb\n-----END TEST-----\n"
 	blocks, err := scanAll(t, block1+block2)
 	require.NoError(t, err)
-	assert.Len(t, blocks, 2)
-	assert.Equal(t, block1, blocks[0])
-	assert.Equal(t, block2, blocks[1])
+	require.Len(t, blocks, 2)
+	require.Equal(t, block1, blocks[0])
+	require.Equal(t, block2, blocks[1])
 }
 
 func TestSplitPEM_CommentsAndWhitespaceBetweenBlocks(t *testing.T) {
 	input := "# comment\n\n-----BEGIN TEST-----\naaa\n-----END TEST-----\n\n# another comment\n\n-----BEGIN TEST-----\nbbb\n-----END TEST-----\n"
 	blocks, err := scanAll(t, input)
 	require.NoError(t, err)
-	assert.Len(t, blocks, 2)
+	require.Len(t, blocks, 2)
 }
 
 func TestSplitPEM_Empty(t *testing.T) {
 	blocks, err := scanAll(t, "")
 	require.NoError(t, err)
-	assert.Empty(t, blocks)
+	require.Empty(t, blocks)
 }
 
 func TestSplitPEM_WhitespaceOnly(t *testing.T) {
 	blocks, err := scanAll(t, "  \n\t\n  ")
 	require.NoError(t, err)
-	assert.Empty(t, blocks)
+	require.Empty(t, blocks)
 }
 
 func TestSplitPEM_TrailingGarbage(t *testing.T) {
 	input := "-----BEGIN TEST-----\ndata\n-----END TEST-----\ngarbage"
 	blocks, err := scanAll(t, input)
-	assert.ErrorIs(t, err, ErrTruncatedPEMBlock)
-	assert.Len(t, blocks, 1)
+	require.ErrorIs(t, err, ErrTruncatedPEMBlock)
+	require.Len(t, blocks, 1)
 }
 
 func TestSplitPEM_TruncatedBlock(t *testing.T) {
 	input := "-----BEGIN TEST-----\npartial data with no end"
 	_, err := scanAll(t, input)
-	assert.ErrorIs(t, err, ErrTruncatedPEMBlock)
+	require.ErrorIs(t, err, ErrTruncatedPEMBlock)
 }
 
 func TestSplitPEM_NoEndNewline(t *testing.T) {
 	input := "-----BEGIN TEST-----\ndata\n-----END TEST-----"
 	blocks, err := scanAll(t, input)
 	require.NoError(t, err)
-	assert.Len(t, blocks, 1)
-	assert.Equal(t, input, blocks[0])
+	require.Len(t, blocks, 1)
+	require.Equal(t, input, blocks[0])
 }
 
 func TestSplitPEM_GarbageOnly(t *testing.T) {
 	_, err := scanAll(t, "this is not PEM data")
-	assert.ErrorIs(t, err, ErrTruncatedPEMBlock)
+	require.ErrorIs(t, err, ErrTruncatedPEMBlock)
 }
