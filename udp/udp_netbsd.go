11afa14ac54398684e764594e573dd8a74353a55
Nate Brown <nbrown.us@gmail.com>
2026-04-22 17:41:54 -0500

    Switch udp to use slog (#1684)

--- udp/udp_netbsd.go
+++ udp/udp_netbsd.go
@@ -11,11 +11,12 @@ import (
 	"net/netip"
 	"syscall"
 
-	"github.com/sirupsen/logrus"
+	"log/slog"
+
 	"golang.org/x/sys/unix"
 )
 
-func NewListener(l *logrus.Logger, ip netip.Addr, port int, multi bool, batch int) (Conn, error) {
+func NewListener(l *slog.Logger, ip netip.Addr, port int, multi bool, batch int) (Conn, error) {
 	return NewGenericListener(l, ip, port, multi, batch)
 }
 
